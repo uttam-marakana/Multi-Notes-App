f435c985c6c673da468d16c689b2d9e588e6d0dd
uttam-marakana <uttammarakana03@gmail.com>
2026-04-09 17:54:24 +0530

    MODIFIED : : "pages" folder file

--- src/pages/BoardManager.jsx
+++ src/pages/BoardManager.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
