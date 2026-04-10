703d3f50c3eb8702b6ec4dc324c42b5b44fee992
uttam-marakana <uttammarakana03@gmail.com>
2026-04-10 14:57:41 +0530

    MODIFIED : : "config" folder file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -19,4 +19,7 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-export const analytics = getAnalytics(app);
+export const analytics =
+  typeof window !== "undefined" && firebaseConfig.measurementId
+    ? getAnalytics(app)
+    : null;
